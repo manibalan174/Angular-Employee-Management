836f92b9b87e6bec4580872c98045003dbedfd8f
Manibalan Sigamani <manibalan0811@hotmail.com>
2026-04-14 22:52:38 +0530

    add employee Controller

---
+++ WebApplication1/Controllers/EmployeeManagementController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebApplication1.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class EmployeeManagementController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetEmployee()
+        {
+            return Ok();
+        }
+    }
+}
